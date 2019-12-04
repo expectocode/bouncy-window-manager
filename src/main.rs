58b9d5b225710e5a3d4aacfa7ce6572bec9a838b
expectocode <expectocode@gmail.com>
2019-12-04 23:52:20 +0000

    assign screen size

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 
-use rand;
+use rand::{self, thread_rng, Rng};
 use xcb;
 
 fn move_window(conn: &xcb::Connection, window: xcb::Window, d_x: i32, d_y: i32) {
@@ -9,6 +9,15 @@ fn move_window(conn: &xcb::Connection, window: xcb::Window, d_x: i32, d_y: i32) 
     let values = [
         (xcb::CONFIG_WINDOW_X as u16, 10),
         (xcb::CONFIG_WINDOW_Y as u16, 10),
+    ];
+    xcb::configure_window(conn, window, &values);
+}
+
+fn resize_window(conn: &xcb::Connection, window: xcb::Window, width: u32, height: u32) {
+    // TODO
+    let values = [
+        (xcb::CONFIG_WINDOW_WIDTH as u16, width),
+        (xcb::CONFIG_WINDOW_HEIGHT as u16, height),
     ];
     xcb::configure_window(conn, window, &values);
 }
@@ -24,18 +33,13 @@ fn assign_speed() -> f32 {
 }
 
 fn assign_size() -> (u32, u32) {
-    (rand::random(), rand::random())
+    let mut rng = thread_rng();
+    (rng.gen_range(20, 100), rng.gen_range(20, 100))
 }
 
 fn main() {
     let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
     dbg!(screen_num);
-
-    let font = conn.generate_id();
-    xcb::open_font(&conn, font, "cursor");
-
-    let cursor = conn.generate_id();
-    xcb::create_glyph_cursor(&conn, cursor, font, font, 0, 30, 0, 0, 0, 0, 0, 0);
 
     let setup = conn.get_setup();
     let screen = setup.roots().nth(screen_num as usize).unwrap();
@@ -52,12 +56,14 @@ fn main() {
             continue;
         }
 
-        for child in children {
-            match windows.entry(child.clone()) {
+        for &child in children {
+            match windows.entry(child) {
                 Entry::Occupied(o) => {
                     dbg!(o.get());
                 }
                 Entry::Vacant(o) => {
+                    let (w, h) = assign_size();
+                    resize_window(&conn, child, w, h);
                     o.insert((assign_direction(), assign_speed()));
                 }
             }
